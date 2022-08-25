2466c2799264a82eec2727ac251bd8a38bb746c4
mshah06 <58752683+mshah06@users.noreply.github.com>
2022-08-25 10:03:43 -0400

    Add files via upload

--- PreprocessingFunctions.py
+++ PreprocessingFunctions.py
@@ -11,12 +11,12 @@ You will need to create folders before running each function. Example usage belo
     
     from PreprocessingFunctions import *
     
-    P1 = '/Users/mshah/Downloads/8_24_Test2/'  <---- Main Folder
-    S1 = '/Users/mshah/Downloads/8_24_Test2/Split/'  <---- Must make this folder
-    F1 = '/Users/mshah/Downloads/8_24_Test2/Filtered/'  <---- Must make this folder
-    M1 = '/Users/mshah/Downloads/8_24_Test2/Masks/'  <---- Must make this folder
-    I1 = '/Users/mshah/Downloads/8_24_Test2/Images/'  <---- Must make this folder
-    
+    #P1 = parent dir
+    buildDirectories(P1, Split = True, Filtered = True, Masks = True, Image = True)
+    S1 = P1 + 'Split/'
+    F1 = P1 + 'Filtered/'
+    M1 = P1 + 'Masks/'
+    I1 = P1 + 'Images/'
     renameFiles(P1)
     convertTIFtoJPG(P1)
     resizeImages(P1)
@@ -36,6 +36,9 @@ import cv2
 from PIL import Image
 import numpy as np
 from numpy import asarray
+from datetime import date
+import matplotlib.pyplot as plt
+
 
 def renameFiles(inputPath, outputPath = "Same"):
     if outputPath == "Same":
@@ -64,13 +67,13 @@ def binarizeMasks(inputPath, outputPath = "Same"):
             M3 = cv2.threshold(M2, thresh, 1, cv2.THRESH_BINARY)[1]
             cv2.imwrite(outputPath + M1, M3)
             
-def resizeImages(inputPath, outputPath = "Same", size = (1500,1500)):
+def resizeImages(inputPath, outputPath = "Same", size = (1500,1500), method = 0):
     if outputPath == "Same":
         outputPath = inputPath
     for I1 in os.listdir(inputPath):
         if I1.endswith(".png") or I1.endswith(".jpg") or I1.endswith(".tif"):
             I2 = Image.open(inputPath + I1)
-            I2 = I2.resize(size)
+            I2 = I2.resize(size, resample=method)
             I2.save(outputPath + I1)
 
 def splitImages(inputPath, outputPath = "Same", size = (150,150)):
@@ -143,3 +146,69 @@ def padFiles(maskInputPath, maskOutputPath = "Same", imgInputPath = "Same", imgO
                 I3 = asarray(I2)
                 I4 = np.pad(I3, ((padSize[0],padSize[1]),(padSize[0],padSize[1]),(0,0)), mode=padding)
                 Image.fromarray(I4).save(imgOutputPath + I1)
+                
+def savePreds(outputPath, array):
+    i = 0
+    for M1 in array:
+        M1 = M1.astype('uint8')
+        S1 = M1.shape
+        A1 = np.ones(S1, dtype='uint8')
+        M2 = A1 - M1
+        Image.fromarray(M2).save(outputPath + 'pred_' + str(i) + '.png')
+        i += 1
+
+def buildDirectories(P1, Split = None, Filtered = None, Masks = None, Image = None):
+    if Split == True:
+        try:
+            os.mkdir(P1 + 'Split/')
+        except FileExistsError:
+            print("Directory exists")
+    if Filtered == True:
+        try:
+            os.mkdir(P1 + 'Filtered/')
+        except FileExistsError:
+            print("Directory exists")
+    if Masks == True:
+        try:
+            os.mkdir(P1 + 'Masks/')
+        except FileExistsError:
+            print("Directory exists")
+    if Image == True:
+        try:
+            os.mkdir(P1 + 'Images/')
+        except FileExistsError:
+            print("Directory exists")
+     
+def saveLossFunction(outputPath, epochs, modelName='history', notes=None):
+    Y1 = date.today().year
+    Y2 = str(Y1)[-2:]
+    M1 = str(date.today().month)
+    M2 = M1.zfill(2)
+    D1 = str(date.today().day)
+    D2 = D1.zfill(2)
+    T1 = Y2+M2+D2
+    P1 = T1 + '_E' + epochs + notes + '.jpg'
+    plt.xlabel("Epochs")
+    plt.plot(modelName.history['loss'], color='b', label="Loss")
+    plt.plot(modelName.history['val_loss'], color='r', label="Validation Loss")
+    plt.legend()
+    plt.savefig(outputPath + P1)
+    plt.show()
+
+def quickUsage(P1):
+    buildDirectories(P1, Split = True, Filtered = True, Masks = True, Image = True)
+    S1 = P1 + 'Split/'
+    F1 = P1 + 'Filtered/'
+    M1 = P1 + 'Masks/'
+    I1 = P1 + 'Images/'
+    renameFiles(P1)
+    convertTIFtoJPG(P1)
+    resizeImages(P1)
+    splitImages(P1, S1)
+    filterBlankImages(S1, F1)
+    binarizeMasks(S1)
+    padFiles(S1)
+    sortImages(S1, M1, I1)
+    
+        
+        
